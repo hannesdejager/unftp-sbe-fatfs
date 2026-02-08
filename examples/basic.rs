6d018b3da6274b6911de2375da986fbee592c67d
Hannes de Jager <hannes.de.jager@gmail.com>
2026-02-08 21:24:58 +0100

    Release v0.1.1

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,4 +1,14 @@
 //! The most basic usage
+//!
+//! This example shows how to serve a FAT filesystem image over FTP.
+//! You can also use a block device (like /dev/sdb1) instead of an image file:
+//! ```rust
+//! let server = ServerBuilder::new(Box::new(move || Vfs::new("/dev/sdb1")))
+//!     .greeting("Welcome to my FAT device over FTP")
+//!     .passive_ports(50000..=65535)
+//!     .build()
+//!     .unwrap();
+//! ```
 
 use libunftp::ServerBuilder;
 use unftp_sbe_fatfs::Vfs;
--- src/lib.rs
+++ src/lib.rs
@@ -463,5 +463,5 @@ fn days_since_1980(year: u16, month: u16, day: u16) -> Option<u32> {
 }
 
 fn is_leap_year(year: u16) -> bool {
-    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+    (year.is_multiple_of(4) && !year.is_multiple_of(100)) || year.is_multiple_of(400)
 }
